abcd499042bee2b98768f33345d7948a8e98490b
Michael Piercy <michael.piercy@epsilon.com>
2022-08-11 17:20:22 +0100

    #7 +Add: Cut Scene -Fix: Reposition Event Listener function and remove on exit.

--- scenes/play.lua
+++ scenes/play.lua
@@ -30,77 +30,12 @@ local settings = {
 -- Scene event functions
 -- -----------------------------------------------------------------------------------
 
--- create()
-function scene:create( event )
-
-    local sceneGroup = self.view
-    -- Code here runs when the scene is first created but has not yet appeared on screen
-
-    -- Create an Info Panel to show details of what tile has been selected
-    infoPanel = Panel:new{message="Select a Tile"}
-    infoPanel.group.x, infoPanel.group.y = display.contentWidth/2, display.contentHeight-display.contentHeight/4
-    sceneGroup:insert(infoPanel.group)
-
-
-    -- Set up array based on settings
-    helper:setArray(settings, HexArray)
-
-    -- Set up display group for hex tiles
-    tileDisplayGroup = helper:setUpTileDisplayGroup()
-
-    -- Populate the HexArray with new Tile instances
-    helper:createRandomHexGrid(HexArray, settings, tileDisplayGroup)
-
-    -- Arrange the display order of the Tile instances based on their Y value
-    helper:sortByDepth(tileDisplayGroup)
-
-end
-
-
--- show()
-function scene:show( event )
-
-    local sceneGroup = self.view
-    local phase = event.phase
-
-    if ( phase == "will" ) then
-        -- Code here runs when the scene is still off screen (but is about to come on screen)
-
-    elseif ( phase == "did" ) then
-        -- Code here runs when the scene is entirely on screen
-
-    end
-end
-
-
--- hide()
-function scene:hide( event )
-
-    local sceneGroup = self.view
-    local phase = event.phase
-
-    if ( phase == "will" ) then
-        -- Code here runs when the scene is on screen (but is about to go off screen)
-
-    elseif ( phase == "did" ) then
-        -- Code here runs immediately after the scene goes entirely off screen
-
-    end
-end
-
-
--- destroy()
-function scene:destroy( event )
-
-    local sceneGroup = self.view
-    -- Code here runs prior to the removal of scene's view
-
-end
-
 --Custom Runtime Listener for event dispatches
 local function customEventRelay(e)
-      if e.name == "activateTile" then
-            local infoPanelUpdate = { name="updateInfo", target=e.target}
+      local target = e.target
+      local name = e.name
+      if name == "activateTile" then
+            local infoPanelUpdate = { name="updateInfo", target=target}
 
 
             --if a tile is already selected
@@ -114,8 +49,8 @@ local function customEventRelay(e)
             end
 
             --if the same tile was selected again, then unhighlight that too
-            if settings.focusTile == e.target then
-                  e.target:unhighlight()
+            if settings.focusTile == target then
+                  target:unhighlight()
                   settings.focusTile = nil
 
                   --reset the info panel details
@@ -125,17 +60,113 @@ local function customEventRelay(e)
 
                   local directions = {"N", "S", "NE", "SE", "NW", "SW"}
                   for i = 1, #directions do
-                        local col, row = e.target:getAdjacent(directions[i])
+                        local col, row = target:getAdjacent(directions[i])
                         if HexArray[col] and HexArray[col][row] then HexArray[col][row]:highlight(0.95, 0.5, 0.25, 0.85) end
                   end
 
-                  e.target:highlight()
+                  target:highlight()
             end
 
             --update the info panel
             infoPanel.group:dispatchEvent( infoPanelUpdate )
       end
 end
+
+-- create()
+function scene:create( event )
+
+      local sceneGroup = self.view
+      -- Code here runs when the scene is first created but has not yet appeared on screen
+
+      -- Create an Info Panel to show details of what tile has been selected
+      infoPanel = Panel:new{message="Select a Tile"}
+      infoPanel.group.x, infoPanel.group.y = display.contentWidth/2, display.contentHeight-display.contentHeight/4
+      sceneGroup:insert(infoPanel.group)
+
+
+end
+
+
+-- show()
+function scene:show( event )
+
+      local sceneGroup = self.view
+      local phase = event.phase
+
+      if ( phase == "will" ) then
+            -- Code here runs when the scene is still off screen (but is about to come on screen)
+
+            -- Set up array based on settings
+            helper:setArray(settings, HexArray)
+
+            -- Set up display group for hex tiles
+            tileDisplayGroup = helper:setUpTileDisplayGroup()
+
+            -- Populate the HexArray with new Tile instances
+            helper:createRandomHexGrid(HexArray, settings, tileDisplayGroup)
+
+            -- Arrange the display order of the Tile instances based on their Y value
+            helper:sortByDepth(tileDisplayGroup)
+
+            sceneGroup:insert(tileDisplayGroup)
+            
+            Runtime:addEventListener("activateTile", customEventRelay)
+
+
+
+      elseif ( phase == "did" ) then
+            -- Code here runs when the scene is entirely on screen
+            -- Function to handle button events
+            local function handleButtonEvent( event )
+
+                  if ( "ended" == event.phase ) then
+                        print( "Button was pressed and released" )
+                        composer.gotoScene( "scenes.cut" )
+                  end
+            end
+
+            local widget = require( "widget" )
+            -- Create the widget
+            local button1 = widget.newButton(
+            {
+                  left = display.contentWidth/2,
+                  top = display.contentHeight-200,
+                  id = "button1",
+                  label = "Restart",
+                  onEvent = handleButtonEvent
+            }
+            )
+      end
+end
+
+
+-- hide()
+function scene:hide( event )
+
+      local sceneGroup = self.view
+      local phase = event.phase
+
+      if ( phase == "will" ) then
+            -- Code here runs when the scene is on screen (but is about to go off screen)
+            Runtime:removeEventListener("activateTile", customEventRelay)
+
+
+      elseif ( phase == "did" ) then
+            -- Code here runs immediately after the scene goes entirely off screen
+
+      end
+end
+
+
+-- destroy()
+function scene:destroy( event )
+
+      local sceneGroup = self.view
+      -- Code here runs prior to the removal of scene's view
+
+end
+
+
 
 
 
@@ -147,7 +178,6 @@ scene:addEventListener( "show", scene )
 scene:addEventListener( "hide", scene )
 scene:addEventListener( "destroy", scene )
 
-Runtime:addEventListener("activateTile", customEventRelay)
 
 -- -----------------------------------------------------------------------------------
 
